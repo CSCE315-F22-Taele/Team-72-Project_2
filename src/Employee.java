b9421a0b3ec9ca23aeb65fa52eb364e4206afc62
Jake Bass <jrbass67@gmail.com>
2022-10-12 18:36:31 -0500

    Employee Class

---
+++ src/Employee.java
@@ -0,0 +1,47 @@
+public class Employee{
+    private int id;
+    private String first_name; 
+    private String last_name;
+    private String username;
+    private String password; 
+    private String role;
+
+
+    public Employee(int _id, String _first, String _last, String _user, 
+    String _pass, String _role){
+        id = _id;
+        first_name = _first;
+        last_name = _last;
+        username = _user;
+        password = _pass;
+        role = _role;
+    }
+
+    public int getId(){
+        return id;
+    }
+    public String getUser(){
+        return username;
+    }
+    public String getPass(){
+        return password;
+    }
+    public String getFirst(){
+        return first_name;
+    }
+    public String getLast(){
+        return last_name;
+    }
+    public String getRole(){
+        return role;
+    }
+
+    @Override
+    public String toString(){
+        String str = "id: " + getId() + ", First Name: " + getFirst() 
+        + ", Last Name: " + getLast() + ", Username: " + getUser()
+        + ", Password: " + getPass() + ", Role: " + getRole();
+        return str;
+    }
+
+}
